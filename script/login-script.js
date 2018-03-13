d417b244a0b1643c0cbddd8bec77730f0cf144da
Jakub Kurs <nfs.kuba@gmail.com>
2018-03-13 14:09:53 +0100

    Revert "Merge branch 'master' of https://github.com/JakubKurs/hotel-prestige"
    This reverts commit d71de6cfe955a97001b156bec3c756b35f031cb6, reversing
    changes made to 01423b8ac1aebe15766379fa67060036d57dd9bf.

--- script/login-script.js
+++
@@ -1,9 +0,0 @@
-
-
-window.onclick = function(event) {
-    if (event.target == document.getElementById("login-start")) 
-        document.body.style.overflow = "hidden";
-
-    else if (event.target == document.getElementById("login-stop"))
-        document.body.style.overflow = "auto";
-}
